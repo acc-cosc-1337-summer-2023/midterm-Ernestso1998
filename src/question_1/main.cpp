3b6b27c3171d6e3e076a6390139334e4b6598e4c
Ernestso1998 <135281894+Ernestso1998@users.noreply.github.com>
2023-07-01 02:42:38 +0000

    Question #1 Changes

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -8,8 +8,8 @@
 int main()
 
 {
-    std::string dna_1 = "GAGCCTACTAACGGGAT" 
-    std::string dna_2 = "CATCGTAATGACGGCCT"
+    std::string dna_1 = "GAGCCTACTAACGGGAT";
+    std::string dna_2 = "CATCGTAATGACGGCCT";
 
 
     get_dna_p_distance(dna_1,dna_2);
